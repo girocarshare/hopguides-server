f46d45aae2c8f46f5216797395958c5712c58c19
Gregor Hočevar <51193034+ghocevar@users.noreply.github.com>
2023-03-13 18:57:20 +0100

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:18.15 as builder
+WORKDIR /usr/src/app
+
+COPY package.json ./
+COPY package-lock.json ./
+RUN npm ci
+
+COPY . .
+RUN npm run build
+
+RUN npm ci --production
+
+FROM gcr.io/distroless/nodejs18-debian11
+WORKDIR /usr/app
+COPY --from=builder /usr/src/app/dist ./dist
+COPY --from=builder /usr/src/app/node_modules ./node_modules
+
+EXPOSE 8000
+CMD ["dist/server.js"]
--- src/models/booking/bookingVehicle.ts
+++ src/models/booking/bookingVehicle.ts
@@ -1,169 +1,169 @@
-import { dbField } from '../../db/decorators';
-import { jsonProperty } from '../../json/decorations';
-import { max, min, validEnum } from '../../validations/decorators';
-import { GeoLocation } from '../address/geoLocation';
-import { Geofence } from '../car/carGeofence';
-import { Maintenance } from '../car/carMaintenance';
-import { VehiclePricing } from '../car/carPricing';
-import { Private } from '../car/carPrivate';
-import { VehicleTracker } from '../car/carTracker';
-import {
-	LockStatus,
-	VehicleAvailable,
-	VehicleStatus,
-	VehicleType,
-	VehicleVersion
-} from '../car/enums';
-import { OperatingMode } from '../tracker/trackerTST100';
-import { PathPoint } from './PathPoint';
+// import { dbField } from '../../db/decorators';
+// import { jsonProperty } from '../../json/decorations';
+// import { max, min, validEnum } from '../../validations/decorators';
+// import { GeoLocation } from '../address/geoLocation';
+// import { Geofence } from '../car/carGeofence';
+// import { Maintenance } from '../car/carMaintenance';
+// import { VehiclePricing } from '../car/carPricing';
+// import { Private } from '../car/carPrivate';
+// import { VehicleTracker } from '../car/carTracker';
+// import {
+// 	LockStatus,
+// 	VehicleAvailable,
+// 	VehicleStatus,
+// 	VehicleType,
+// 	VehicleVersion
+// } from '../car/enums';
+// import { OperatingMode } from '../tracker/trackerTST100';
+// import { PathPoint } from './PathPoint';
 
-export class StateInfo {
-	@jsonProperty()
-	@dbField()
-	time: number;
+// export class StateInfo {
+// 	@jsonProperty()
+// 	@dbField()
+// 	time: number;
 
-	@jsonProperty()
-	@dbField()
-	lockStatus: LockStatus;
+// 	@jsonProperty()
+// 	@dbField()
+// 	lockStatus: LockStatus;
 
-	@jsonProperty()
-	@dbField()
-	location: GeoLocation;
+// 	@jsonProperty()
+// 	@dbField()
+// 	location: GeoLocation;
 
-	@jsonProperty()
-	@dbField()
-	device: GeoLocation;
+// 	@jsonProperty()
+// 	@dbField()
+// 	device: GeoLocation;
 
-	@jsonProperty()
-	@dbField()
-	@min(-1)
-	@max(100)
-	battery: number = -1;
+// 	@jsonProperty()
+// 	@dbField()
+// 	@min(-1)
+// 	@max(100)
+// 	battery: number = -1;
 
-	@jsonProperty()
-	@dbField()
-	@min(-1)
-	@max(100)
-	fuel: number = -1;
+// 	@jsonProperty()
+// 	@dbField()
+// 	@min(-1)
+// 	@max(100)
+// 	fuel: number = -1;
 
-	@jsonProperty()
-	@dbField()
-	mileage: number;
+// 	@jsonProperty()
+// 	@dbField()
+// 	mileage: number;
 
-	@jsonProperty()
-	@dbField()
-	speedLimit: number;
+// 	@jsonProperty()
+// 	@dbField()
+// 	speedLimit: number;
 
-	@jsonProperty()
-	@dbField()
-	operatingMode: OperatingMode;
+// 	@jsonProperty()
+// 	@dbField()
+// 	operatingMode: OperatingMode;
 
-	@jsonProperty()
-	@dbField()
-	doorStatus: number;
+// 	@jsonProperty()
+// 	@dbField()
+// 	doorStatus: number;
 
-	@jsonProperty()
-	@dbField()
-	imobilized: boolean;
+// 	@jsonProperty()
+// 	@dbField()
+// 	imobilized: boolean;
 
-	@jsonProperty()
-	@dbField()
-	image: string;
+// 	@jsonProperty()
+// 	@dbField()
+// 	image: string;
 
-	@jsonProperty()
-	@dbField()
-	geoName: string;
+// 	@jsonProperty()
+// 	@dbField()
+// 	geoName: string;
 
-	@jsonProperty()
-	@dbField()
-	country: string;
+// 	@jsonProperty()
+// 	@dbField()
+// 	country: string;
 
-	@jsonProperty()
-	@dbField()
-	signalTime: number;
-}
+// 	@jsonProperty()
+// 	@dbField()
+// 	signalTime: number;
+// }
 
-export class VehicleRentState {
-	@jsonProperty()
-	@dbField()
-	start: StateInfo;
+// export class VehicleRentState {
+// 	@jsonProperty()
+// 	@dbField()
+// 	start: StateInfo;
 
-	@jsonProperty()
-	@dbField()
-	end: StateInfo;
-}
+// 	@jsonProperty()
+// 	@dbField()
+// 	end: StateInfo;
+// }
 
-export class BookingVehicle {
-	@jsonProperty()
-	@dbField()
-	id: string;
+// export class BookingVehicle {
+// 	@jsonProperty()
+// 	@dbField()
+// 	id: string;
 
-	@jsonProperty()
-	@dbField()
-	IMEI: string;
+// 	@jsonProperty()
+// 	@dbField()
+// 	IMEI: string;
 
-	@jsonProperty()
-	@dbField()
-	userId: string;
+// 	@jsonProperty()
+// 	@dbField()
+// 	userId: string;
 
-	@jsonProperty()
-	@dbField()
-	type: VehicleType;
+// 	@jsonProperty()
+// 	@dbField()
+// 	type: VehicleType;
 
-	@jsonProperty()
-	@dbField()
-	status: VehicleStatus;
+// 	@jsonProperty()
+// 	@dbField()
+// 	status: VehicleStatus;
 
-	@jsonProperty()
-	@dbField()
-	available: VehicleAvailable;
+// 	@jsonProperty()
+// 	@dbField()
+// 	available: VehicleAvailable;
 
-	@jsonProperty()
-	@dbField()
-	pricing: VehiclePricing;
+// 	@jsonProperty()
+// 	@dbField()
+// 	pricing: VehiclePricing;
 
-	@jsonProperty()
-	@dbField()
-	maintainedBy: Maintenance;
+// 	@jsonProperty()
+// 	@dbField()
+// 	maintainedBy: Maintenance;
 
-	@jsonProperty()
-	@dbField()
-	private: Private;
+// 	@jsonProperty()
+// 	@dbField()
+// 	private: Private;
 
-	@jsonProperty()
-	@dbField()
-	geofence: Geofence;
+// 	@jsonProperty()
+// 	@dbField()
+// 	geofence: Geofence;
 
-	@jsonProperty()
-	@dbField()
-	modifiedAt: number;
+// 	@jsonProperty()
+// 	@dbField()
+// 	modifiedAt: number;
 
-	@jsonProperty({ type: PathPoint })
-	@dbField({ type: PathPoint })
-	@validEnum(PathPoint)
-	path: PathPoint[];
+// 	@jsonProperty({ type: PathPoint })
+// 	@dbField({ type: PathPoint })
+// 	@validEnum(PathPoint)
+// 	path: PathPoint[];
 
-	@jsonProperty()
-	@dbField()
-	lockCode: string;
+// 	@jsonProperty()
+// 	@dbField()
+// 	lockCode: string;
 
-	@jsonProperty()
-	@dbField()
-	title: string;
+// 	@jsonProperty()
+// 	@dbField()
+// 	title: string;
 
-	@jsonProperty()
-	@dbField()
-	version: VehicleVersion;
+// 	@jsonProperty()
+// 	@dbField()
+// 	version: VehicleVersion;
 
-	@jsonProperty()
-	@dbField()
-	serialNumber: string;
+// 	@jsonProperty()
+// 	@dbField()
+// 	serialNumber: string;
 
-	@jsonProperty()
-	@dbField()
-	trackerInfo: VehicleTracker;
+// 	@jsonProperty()
+// 	@dbField()
+// 	trackerInfo: VehicleTracker;
 
-	@jsonProperty()
-	@dbField()
-	state: VehicleRentState;
-}
+// 	@jsonProperty()
+// 	@dbField()
+// 	state: VehicleRentState;
+// }
