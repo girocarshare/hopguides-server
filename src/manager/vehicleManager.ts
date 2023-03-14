9bf5d992f33f5ffccbc09578008befed0039025e
Gregor Hočevar <51193034+ghocevar@users.noreply.github.com>
2023-03-14 09:57:33 +0100

    Fix imports

--- src/manager/vehicleManager.ts
+++ src/manager/vehicleManager.ts
@@ -1,33 +1,38 @@
-
 import { CreateVehiclePayload } from '../classes/vehicle/createVehiclePayload';
 import { CustomError } from '../classes/customError';
-import VehicleRepo, { VehicleRepository } from '../db/repository/vehicleRepository';
+import VehicleRepo, {
+  VehicleRepository,
+} from '../db/repository/vehicleRepository';
 import { deserialize } from '../json';
 import { Vehicle } from '../models/vehicle/vehicle';
-import { VehicleLiveInfo } from '../models/vehicle/LiveInfo';
+import { VehicleLiveInfo } from '../models/vehicle/liveInfo';
 
 export class VehicleManager {
-	vehicleRepository: VehicleRepository;
+  vehicleRepository: VehicleRepository;
 
-	constructor() {
-		this.vehicleRepository = VehicleRepo;
-	}
+  constructor() {
+    this.vehicleRepository = VehicleRepo;
+  }
 
-	async createVehicle(vehicle: Vehicle): Promise<Vehicle> {
-		return await this.vehicleRepository.createOne(vehicle).catch(() => {
-			throw new CustomError(500, 'Vehicle not created!');
-		});
-	}
+  async createVehicle(vehicle: Vehicle): Promise<Vehicle> {
+    return await this.vehicleRepository.createOne(vehicle).catch(() => {
+      throw new CustomError(500, 'Vehicle not created!');
+    });
+  }
 
-	async create(userId: string, vehicleData: CreateVehiclePayload): Promise<Vehicle> {
-		const payload: Vehicle = deserialize(Vehicle, vehicleData);
-		payload.liveInfo = new VehicleLiveInfo();
+  async create(
+    userId: string,
+    vehicleData: CreateVehiclePayload
+  ): Promise<Vehicle> {
+    const payload: Vehicle = deserialize(Vehicle, vehicleData);
+    payload.liveInfo = new VehicleLiveInfo();
 
-		const vehicle: Vehicle = await this.vehicleRepository.createOne(payload).catch(() => {
-			throw new Error('Error creating a Vehicle');
-		});
+    const vehicle: Vehicle = await this.vehicleRepository
+      .createOne(payload)
+      .catch(() => {
+        throw new Error('Error creating a Vehicle');
+      });
 
-		return vehicle;
-	}
-
+    return vehicle;
+  }
 }
