193b75c369734309c8f6614bb54fe5890e003a52
luna-zivkovic <luna.zivkovic@gogiro.app>
2023-03-23 14:34:42 +0100

    different get tours

---
+++ src/classes/tour/pointData.ts
@@ -0,0 +1,35 @@
+
+class Location {
+	lat: string;
+
+	lng: string;
+
+}
+
+
+export class PointData {
+	id: string;
+
+	name: string;
+
+	location: Location;
+
+	shortInfo: string;
+
+	longInfo: string;
+
+	images: string[];
+
+	offerName: string;
+
+	category: string;
+
+	audio: string;
+
+	voucher: string;
+
+	hasVoucher: Boolean;
+
+	voucherDesc: string;
+}
+
