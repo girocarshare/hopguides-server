754490b2e064e7bdd97b7f92e689b1572d4b66f2
lunazivkovic-giro <luna.zivkovic@gogiro.app>
2023-12-18 14:48:24 +0100

    new way of creating video

--- src/manager/libraryManager.ts
+++ src/manager/libraryManager.ts
@@ -15,6 +15,7 @@ import axios from 'axios';
 import libraryRepository, { LibraryRepository } from '../db/repository/libraryRepository';
 import { Library } from '../models/library/library';
 
+import * as fs from 'fs';
 var QRCode = require('qrcode')
 
 
@@ -27,101 +28,101 @@ export class LibraryManager {
 
 
 	libraryRepository: LibraryRepository;
-	
+
 	constructor() {
 		this.libraryRepository = libraryRepository;
-	
+
 	}
 
 
 
 	async generateQr(url: string): Promise<string> {
 
-		
-			const qrCodeId = Date.now() + "-" + Math.floor(Math.random() * 1000);
 
-			var opts = {
-				margin: 1,
-				color: {
-					dark: "#010599FF",
-					light: "#FFBF60FF"
-				}
+		const qrCodeId = Date.now() + "-" + Math.floor(Math.random() * 1000);
+
+		var opts = {
+			margin: 1,
+			color: {
+				dark: "#010599FF",
+				light: "#FFBF60FF"
 			}
-			await QRCode.toDataURL(url, {
-				scale: 15,
-				width: "1000px",
-			}, async function (err, base64) {
-				const base64Data: Buffer = Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ""), 'base64');
-				const type = base64.split(';')[0].split('/')[1];
-				const params = {
-					Bucket: 'hopguides/library',
-					Key: `${qrCodeId}.png`, // type is not required
-					Body: base64Data,
-					ACL: 'public-read',
-					ContentEncoding: 'base64', // required
-					ContentType: `image/${type}` // required. Notice the back ticks
+		}
+		await QRCode.toDataURL(url, {
+			scale: 15,
+			width: "1000px",
+		}, async function (err, base64) {
+			const base64Data: Buffer = Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+			const type = base64.split(';')[0].split('/')[1];
+			const params = {
+				Bucket: 'hopguides/library',
+				Key: `${qrCodeId}.png`, // type is not required
+				Body: base64Data,
+				ACL: 'public-read',
+				ContentEncoding: 'base64', // required
+				ContentType: `image/${type}` // required. Notice the back ticks
+			}
+			s3bucket.upload(params, function (err, data) {
+
+				if (err) {
+					console.log('ERROR MSG: ', err);
+				} else {
+					console.log('Successfully uploaded data');
+					console.log(data);
 				}
-				s3bucket.upload(params, function (err, data) {
-
-					if (err) {
-						console.log('ERROR MSG: ', err);
-					} else {
-						console.log('Successfully uploaded data');
-						console.log(data);
-					}
-				});
-
 			});
 
-		
+		});
+
+
 		return `https://hopguides.s3.eu-central-1.amazonaws.com/library/${qrCodeId}.png`
 
 		//}
 	}
 
 
-	
+
 
 	async saveGeneratedVideo(url: string): Promise<string> {
-  const videoName = Date.now() + "-" + Math.floor(Math.random() * 1000);
+		const videoName = Date.now() + "-" + Math.floor(Math.random() * 1000);
 
-  // Fetch the video stream from the URL
-  const response = await axios.get(url, {
-    responseType: 'stream',
-  });
+		// Fetch the video stream from the URL
+		const fileStream = fs.createReadStream(url);
+		// Create the S3 upload parameters
+		const params = {
+			Bucket: 'hopguides/library',
+			Key: `${videoName}.mp4`,
+			Body: fileStream,
+			ACL: 'public-read',
+			ContentType: 'video/mp4',
+		};
 
-  // Create the S3 upload parameters
-  const params = {
-    Bucket: 'hopguides/library',
-    Key: `${videoName}.mp4`,
-    Body: response.data,
-    ACL: 'public-read',
-    ContentType: response.headers['content-type'],
-  };
+		// Upload the video stream to S3
+		await new Promise((resolve, reject) => {
+			s3bucket.upload(params, (err, data) => {
+				if (err) {
+					console.log('ERROR MSG: ', err);
+					reject(err);
+				} else {
+					console.log('Successfully uploaded data');
+					console.log(data);
+					resolve(`https://hopguides.s3.eu-central-1.amazonaws.com/library/${videoName}.mp4`);
+				}
+			});
+		});
 
-  // Upload the video stream to S3
-  return new Promise((resolve, reject) => {
-    s3bucket.upload(params, (err, data) => {
-      if (err) {
-        console.log('ERROR MSG: ', err);
-        reject(err);
-      } else {
-        console.log('Successfully uploaded data');
-        console.log(data);
-        resolve(`https://hopguides.s3.eu-central-1.amazonaws.com/library/${videoName}.mp4`);
-      }
-    });
-  });
-}
+		return `https://hopguides.s3.eu-central-1.amazonaws.com/library/${videoName}.mp4`
+	}
 
 
+	
 
 	async create(library: Library): Promise<Library> {
 		// search for user, check if it exists, if it does, check for the fields of confirmed and createdAt
-		var lib:Library = await this.libraryRepository.createOne(library)
+		var lib: Library = await this.libraryRepository.createOne(library)
 
 		return lib;
-		
+
 	}
 
 
